ec347994c6c99acf3496f3f807e8f6af47a0c4b6
Your Name <yohai109@gmail.com>
2022-02-19 16:18:47 +0200

    the images are now loaded by picasso

--- app/src/main/java/com/example/thegadgetapp/newsfeed/NewsViewHolder.java
+++ app/src/main/java/com/example/thegadgetapp/newsfeed/NewsViewHolder.java
@@ -1,18 +1,28 @@
 package com.example.thegadgetapp.newsfeed;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.thegadgetapp.R;
 import com.example.thegadgetapp.database.entities.Article;
+import com.squareup.picasso.Picasso;
 
 public class NewsViewHolder extends RecyclerView.ViewHolder {
     Article currArticle;
-
+    TextView header;
+    TextView secondaryHeader;
+    ImageView imageView;
 
     public NewsViewHolder(@NonNull View itemView, ViewHolderOnClickListener clickListener) {
         super(itemView);
+
+        header = itemView.findViewById(R.id.header);
+        secondaryHeader = itemView.findViewById(R.id.secondary_header);
+        imageView = itemView.findViewById(R.id.image);
         itemView.setOnClickListener(v -> {
             clickListener.onclick(itemView, currArticle);
         });
@@ -20,6 +30,13 @@ public class NewsViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(Article newArticle) {
         currArticle = newArticle;
+        header.setText(currArticle.header);
+        secondaryHeader.setText(currArticle.secondaryHeader);
+        Picasso.get()
+                .load(currArticle.imageUri)
+                .resize(120, 120)
+                .centerCrop()
+                .into(imageView);
     }
 
     public interface ViewHolderOnClickListener {
