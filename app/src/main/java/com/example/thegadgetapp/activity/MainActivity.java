671f95370e994dea7c482f3b3b97b4d521b1655c
Your Name <yohai109@gmail.com>
2022-02-19 14:47:32 +0200

    add global logout button

--- app/src/main/java/com/example/thegadgetapp/activity/MainActivity.java
+++ app/src/main/java/com/example/thegadgetapp/activity/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -17,17 +18,32 @@ import com.example.thegadgetapp.ViewModelFactory;
 public class MainActivity extends AppCompatActivity {
     private ActivityViewModel viewModel;
     public String currUserId;
+    Toolbar toolbar;
+
+    private Boolean isLogoutBtnShown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ViewModelFactory factory = ((MainApplication) getApplication()).getFactory();
         viewModel = new ViewModelProvider(this, factory).get(ActivityViewModel.class);
         viewModel.initDB();
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        initToolbar();
+        initNavigation();
+    }
 
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.main_menu);
+        isLogoutBtnShown = false;
+
+
+    }
+
+    private void initNavigation() {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
 
@@ -41,9 +57,27 @@ public class MainActivity extends AppCompatActivity {
                 navController,
                 appBarConfiguration
         );
+
+        setLogoutClickListener(navController);
+    }
+
+    private void setLogoutClickListener(NavController navController){
+        toolbar.getMenu().findItem(R.id.logout).setOnMenuItemClickListener(item -> {
+            viewModel.logout();
+            navController.navigate(R.id.action_global_loginFragment);
+            return true;
+        });
     }
 
     public ViewModelFactory getFactory() {
         return ((MainApplication) getApplication()).getFactory();
+    }
+
+    public void toggleLogoutButton(Boolean shouldShow) {
+        if (shouldShow != isLogoutBtnShown) {
+            isLogoutBtnShown = shouldShow;
+            toolbar.getMenu().findItem(R.id.logout).setVisible(isLogoutBtnShown);
+            toolbar.invalidateMenu();
+        }
     }
 }
